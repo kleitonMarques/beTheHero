0c388eaaf6f03fd39040d91ce4e3d52ab6596d1a
Kleiton Marques <kleitonm.marques@gmail.com>
2020-03-26 12:30:30 -0300

    Conceito de PROPS e STATE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header'
 
 function App() {
-  return (
-    <h1>Hello Word</h1>
-  );
+	let [counter, setCounter] = useState(0)
+
+	function increment() {
+		setCounter(counter + 1)
+	}
+
+	return (
+		<div>
+			<Header>Contador: {counter}</Header>
+			<button onClick={increment}>Incrementar</button>
+		</div>
+	);
 }
 
 export default App;
