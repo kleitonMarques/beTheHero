4600dcd041794a44a1caa5a2769e5218c46410b2
Kleiton Marques <kleitonm.marques@gmail.com>
2020-03-27 15:06:22 -0300

    Cadastrando novo caso

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
@@ -7,6 +7,7 @@ import api from '../../service/api'
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([])
+    const history = useHistory()
     const ongId = localStorage.getItem('ongId')
     const ongName = localStorage.getItem('ongName')
 
@@ -33,13 +34,18 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear()
+        history.push('')
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Heroe" />
                 <span>Bem Vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
