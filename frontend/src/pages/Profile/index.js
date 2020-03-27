66e9fd0941f737ffc1a9797723b8e7536d29516f
Kleiton Marques <kleitonm.marques@gmail.com>
2020-03-27 14:10:46 -0300

    Delete casos na API

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -20,6 +20,19 @@ export default function Profile() {
         })
     }, [ongId])
 
+    async function handleDeleteIncident(id){
+        try {
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            })
+            setIncidents(incidents.filter(incident => incident.id !== id))
+        } catch {
+            alert('Erro ao deletar caso, tente novamente.')
+        }
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -41,7 +54,7 @@ export default function Profile() {
                     <p>{incident.description}</p>
                     <strong>VALOR:</strong>
                     <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
-                    <button type="button">
+                    <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                         <FiTrash2 size={20} color="#a8a8b3" />
                     </button>
                 </li>
