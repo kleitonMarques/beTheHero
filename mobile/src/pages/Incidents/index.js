e8fa94c0fde32a7e546015bc7bffb7697d9b69e6
Kleiton Marques <kleitonm.marques@gmail.com>
2020-03-29 12:41:36 -0300

    CARREGAR MAIS CASOS DA LISTA QUANDO CHEGAR NO FINAL DA TELA

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -9,6 +9,10 @@ import api from '../../services/api'
 export default function Incidents() {
     const [incidents, setIncidents] = useState([])
     const [total, setTotal] = useState(0)
+
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+
     const navigation = useNavigation()
 
     function navigateToDetail(incident) {
@@ -16,9 +20,23 @@ export default function Incidents() {
     }
 
     async function loadIncidents(){
-        const response = await api.get('incidents')
-        setIncidents(response.data)
+        if(loading) {
+            return;
+        }
+
+        if(total > 0 && incidents.length === total) {
+            return;
+        }
+
+        setLoading(true)
+
+        const response = await api.get('incidents', {
+            params: { page }
+        })
+        setIncidents([...incidents, ...response.data])
         setTotal(response.headers['x-total-count'])
+        setPage(page + 1)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -37,7 +55,13 @@ export default function Incidents() {
             <Text style={styles.title}>Bem vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
 
-            <FlatList data={incidents} style={styles.incidentList} keyExtractor={incident => String(incident.id)} showsVerticalScrollIndicator={false} renderItem={({ item: incident }) => (
+            <FlatList data={incidents}
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident.id)}
+                showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
+                renderItem={({ item: incident }) => (
                 <View style={styles.incident}>
                     <Text style={styles.incidentProperty}>ONG:</Text>
                     <Text style={styles.incidentValue}>{incident.name}</Text>
