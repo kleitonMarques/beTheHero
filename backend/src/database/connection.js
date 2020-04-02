576d4fd74a15a92fb7230afeccf9c6501824cb86
Kleiton Marques <kleitonm.marques@gmail.com>
2020-04-02 18:49:03 -0300

    Teste integrado no Back-end

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configurantion = require('../../knexfile');
 
-const connection = knex(configurantion.development);
+const config = process.env.NODE_ENV === 'test' ? configurantion.test : configurantion.development
+
+const connection = knex(config);
 
 module.exports = connection;
